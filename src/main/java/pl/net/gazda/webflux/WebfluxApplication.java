3479edbed0ff0df70e06ec635ce8c3d54167367a
Lukasz Gazda <lukasz.gazda@ocado.com>
2019-10-20 20:38:04 +0200

    next steps

--- src/main/java/pl/net/gazda/webflux/WebfluxApplication.java
+++ src/main/java/pl/net/gazda/webflux/WebfluxApplication.java
@@ -1,6 +1,11 @@
 package pl.net.gazda.webflux;
 
-import com.google.common.collect.ImmutableList;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,13 +14,17 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.client.WebClient;
+import pl.net.gazda.webflux.WebfluxApplication.MultiDataResult.MultiDataResultBuilder;
+import pl.net.gazda.webflux.WebfluxApplication.MultiDataResult.NumericData;
+import pl.net.gazda.webflux.WebfluxApplication.MultiDataResult.TimeData;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import static java.util.Collections.nCopies;
@@ -39,56 +48,63 @@ public class WebfluxApplication {
 		return WebClient.builder();
 	}
 
+
 	@GetMapping("/mono-zip")
-	@ResponseBody
-	public Mono<List<String>> mono() {
-		return Mono.just(ImmutableList.<String>builder())
-				.zipWith(webClientGetRequest("/status/200/delay/1"), ImmutableList.Builder::add)
-				.zipWith(webClientGetRequest("/status/200/delay/2"), ImmutableList.Builder::add)
-				.zipWith(webClientGetRequest("/status/400/delay/1"), ImmutableList.Builder::add)
-				.zipWith(webClientGetRequest("/unknown"), ImmutableList.Builder::add)
-				.map(ImmutableList.Builder::build);
+	public Mono<?> mono() {
+		return Mono.just(MultiDataResult.builder())
+				.zipWith(webClientGet("/numeric").bodyToMono(NumericData.class), MultiDataResultBuilder::number)
+				.zipWith(webClientGet("/time").bodyToMono(TimeData.class), MultiDataResultBuilder::time)
+				.map(MultiDataResultBuilder::build);
 
 	}
 
-    @GetMapping("/flux-concat")
-    @ResponseBody
+    @GetMapping("/flux-flatmap")
 	public Mono<?> concatMap() {
-        return Flux.fromIterable(nCopies(50, "/status/200/delay/1"))
-            .flatMap(this::webClientGetRequest, 10)
+        return Flux.fromIterable(nCopies(50, "/time"))
+            .flatMap(url -> webClientGet(url).bodyToMono(TimeData.class), 10)
             .doOnEach(signal -> log.info("{}", signal))
-            .reduce(String::concat);
+            .collect(LinkedList::new, List::add);
 
     }
 
 	@GetMapping("/flux-merge")
-	public Flux<String> flux() {
+	public Flux<?> flux() {
 		return Flux.merge(
-				webClientGetRequest("/status/200/delay/1"),
-				webClientGetRequest("/status/200/delay/2"),
-				webClientGetRequest("/status/400/1"),
-				webClientGetRequest("/unknown")
+				webClientGet("/numeric").bodyToMono(NumericData.class),
+				webClientGet("/numeric").bodyToMono(NumericData.class),
+				webClientGet("/400").bodyToMono(NumericData.class).onErrorReturn(NumericData.ZERO),
+				webClientGet("/unknown").bodyToMono(NumericData.class).onErrorResume(throwable -> Mono.empty())
 		);
 	}
 
-	@GetMapping("/flux-reduce")
-	public Mono<String> fluxReduceToMono() {
-		return Flux.merge(
-				webClientGetRequest("/status/200/delay/2"),
-				webClientGetRequest("/status/200/delay/1"),
-				webClientGetRequest("/status/400/1"),
-				webClientGetRequest("/unknown"))
-			.collect(StringBuilder::new, (stringBuilder, s) -> stringBuilder.append(s).append(" \n"))
-			.map(StringBuilder::toString);
-	}
-
-
-	private Mono<String> webClientGetRequest(String path) {
+	private WebClient.ResponseSpec webClientGet(String path) {
 		return webClientBuilder().build()
 				.get()
 				.uri(uriBuilder -> uriBuilder.scheme("http").host("wiremock").path(path).build())
-				.retrieve()
-				.bodyToMono(String.class)
-				.onErrorResume(throwable -> Mono.just(throwable.toString()));
+				.retrieve();
+	}
+
+	@Value
+	@Builder
+	public static class MultiDataResult {
+		private final TimeData time;
+		private final NumericData number;
+
+		@Data
+		@NoArgsConstructor
+		public static class TimeData {
+			@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+			private Date time;
+		}
+
+		@Data
+		@NoArgsConstructor
+		@Builder
+		@AllArgsConstructor
+		public static class NumericData {
+			private Number number;
+
+			public static final NumericData ZERO = NumericData.builder().number(0).build();
+		}
 	}
 }
